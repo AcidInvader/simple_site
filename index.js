1a3decc7472921512ab3c91169a7c871669573bb
AcidInvader <124760286+AcidInvader@users.noreply.github.com>
2023-11-26 11:06:38 +0300

    modules

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const CC = require("currency-converter-lt")
+const os = require("os")
+const math = require("./math.js")
+
+let result1 = math.add(2, 5);
+let result2 = math.multy(2, 5);
+let platform = os.platform()
 let currencyConverter = new CC({from: "USD",  to: "RUB", amount: 100})
 
 currencyConverter.convert().then((response) => {
-    console.log(`Result: ${response}`)
+    console.log(`Result_converter: ${response}`)
 })
 
+console.log("Your platform:", platform)
+console.log(`result: ${result1}`);
+console.log(`result_multy: ${result2}`);
---
+++ math.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const multy = (a, b) => {
+    return a * b
+}
+
+module.exports = {
+    add: add,
+    multy: multy,
+}
